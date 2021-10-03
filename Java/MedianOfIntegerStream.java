bb0c668323e0dfe275a22fa22c0328725a42deef
Dheeraj Gupta <36954757+dheerajgupta217@users.noreply.github.com>
2021-10-03 08:19:27 +0530

    Create MedianOfIntegerStream.java

---
+++ Java/MedianOfIntegerStream.java
@@ -0,0 +1,35 @@
+class MedianOfIntegerStream {
+
+    private Queue<Integer> minHeap, maxHeap;
+
+    MedianOfIntegerStream() {
+        minHeap = new PriorityQueue<>();
+        maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+    }
+
+    void add(int num) {
+        if (!minHeap.isEmpty() && num < minHeap.peek()) {
+            maxHeap.offer(num);
+            if (maxHeap.size() > minHeap.size() + 1) {
+                minHeap.offer(maxHeap.poll());
+            }
+        } else {
+            minHeap.offer(num);
+            if (minHeap.size() > maxHeap.size() + 1) {
+                maxHeap.offer(minHeap.poll());
+            }
+        }
+    }
+
+    double getMedian() {
+        int median;
+        if (minHeap.size() < maxHeap.size()) {
+            median = maxHeap.peek();
+        } else if (minHeap.size() > maxHeap.size()) {
+            median = minHeap.peek();
+        } else {
+            median = (minHeap.peek() + maxHeap.peek()) / 2; 
+        }
+        return median;
+    }
+}
